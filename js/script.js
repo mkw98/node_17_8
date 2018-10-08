027de8fd1dc1414acf66263d0fa461936ddb8442
Maria Wieckowska <mkw98@gazeta.pl>
2018-10-09 01:52:39 +0200

    edit script

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ server.on('request', function (request, response) {
     response.setHeader("Content-Type", "text/html; charset=utf-8");
     if (request.method === 'GET' && request.url === '/') {
 		fs.readFile( '../index.html', 'utf-8', function( err, data ) {
+        //response.write (tekst do wyswietlenia)
         response.write(data);
         if(err) throw err;
         response.end();
