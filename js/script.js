4f93dbdba373e79bf49820cbd06a3a268f834c61
Maria Wieckowska <mkw98@gazeta.pl>
2018-10-09 01:50:17 +0200

    edit files

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-'use scrict';
+'use script';
 
 var http = require('http');
 var fs = require( 'fs' );
@@ -9,21 +9,20 @@ server.on('request', function (request, response) {
     response.setHeader("Content-Type", "text/html; charset=utf-8");
     if (request.method === 'GET' && request.url === '/') {
 		fs.readFile( '../index.html', 'utf-8', function( err, data ) {
-        response.write('ten tekst powinien sie pojawic');
-        if (!err) response.write('ten tekst powinien sie pojawic?');
+        response.write(data);
+        if(err) throw err;
         response.end();
     });
            	        
     } else {
             fs.readFile('../images/404-error-page-not-found.jpg', function (err, data) {
-            response.statusCode = 404;
-            response.write('../images/404-error-page-not-found.jpg ');
-            if (!err) response.write('ten tekst powinien sie pojawic?');
-            response.end();
-    });
+                response.statusCode = 404;
+                response.write(data);
+                if(err) throw err;
+                response.end();
+     });
    }
 });
 
 server.listen(8080);
-
 
