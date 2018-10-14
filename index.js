398bc8b06910cc109ff5456656cd0959f0d446e9
mkw98 <40563214+mkw98@users.noreply.github.com>
2018-10-14 10:36:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
-'use script';
-
 var http = require('http');
 var fs = require( 'fs' );
 
 var server = http.createServer();
 
-server.on('request', function (request, response) {
-    response.setHeader("Content-Type", "text/html; charset=utf-8");
+server.on('request', function (request, response) {    
     if (request.method === 'GET' && request.url === '/') {
-		fs.readFile( 'index.html', 'utf-8', function( err, data ) {
-        //response.write (tekst do wyswietlenia)
-        response.write(data);
-        if(err) throw err;
-        response.end();
-    });
-           	        
+        response.setHeader("Content-Type", "text/html; charset=utf-8");
+	fs.readFile( 'index.html', 'utf-8', function(err, data) {
+            response.write(data);
+            if(err) throw err;
+            response.end();
+        });   	        
     } else {
-            fs.readFile('404-error-page-not-found.jpg', function (err, data) {
-                response.statusCode = 404;
-                response.write(data);
-                if(err) throw err;
-                response.end();
-     });
-   }
+        response.setHeader("Content-Type", "image/jpeg");
+        fs.readFile('404-error-page-not-found.jpg', function (err, data) {
+            response.statusCode = 404;
+            response.write(data);
+            if(err) throw err;
+            response.end();
+        });
+    }
 });
 
 server.listen(8080);
